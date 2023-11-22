cc1d74e7f2e1b2c813eed28f51c578765bb63da8
tildeholmqvist <>
2023-11-22 12:27:23 +0000

    git commit -m added my submit button to the frontpage and styled it so it appears on top of the image

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+    const startButton = document.getElementById("start-quiz");
+    // Redirect to the quizgame page when the button is clicked
+    startButton.addEventListener('click', function () {
+        window.location.href = 'quiz.html';
+    });
+};
+const submitButton = document.getElementById("submit-button");
+submitButton.addEventListener('click', function () {
+
+});
