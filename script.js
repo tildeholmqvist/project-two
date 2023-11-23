c28ab49e128dcc8f43a044b8d0b7bf74e1d577a6
tildeholmqvist <>
2023-11-23 11:52:01 +0000

    tried to change the questions to a slideshow with js and css

--- script.js
+++ script.js
@@ -91,6 +91,12 @@ let quizQuestions = [
     },
 ];
 
+/**
+ * This code is from https://www.sitepoint.com/simple-javascript-quiz/
+ * I took help from that site to get the result that I wished for, since the 
+ * quiz wasn't working how I wanted it too.. 
+ * I wished for the questions to not be on top of eachother, but in a loop, this was the most convinent way I found
+ */
 function buildQuiz() {
     const quizContainer = document.getElementById('quiz');
     let output = '';
@@ -111,30 +117,70 @@ function buildQuiz() {
     });
 
     quizContainer.innerHTML = output;
-}
 
-function showResults() {
-    const quizContainer = document.getElementById('quiz');
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-    let numCorrect = 0;
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
 
-    quizQuestions.forEach((currentQuestion, questionNumber) => {
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainers[questionNumber].querySelector(selector) || {}).value;
+    function showSlide(n) {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
 
-        if (userAnswer === currentQuestion.correctAnswer) {
-            numCorrect++;
-            alert(`Good Job! You got it right!`);
+        let previousButton = document.getElementById('previous');
+        let nextButton = document.getElementById('next');
+        let submitButton = document.getElementById('submit');
+
+        if (currentSlide === 0) {
+            previousButton.style.display = 'none';
         } else {
-            answerContainers[questionNumber].alert(`That is not the right answer, try again!`);
+            previousButton.style.display = 'inline-block';
         }
-    });
 
-    const resultsContainer = document.getElementById('result');
-    resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+        if (currentSlide === slides.length - 1) {
+            nextButton.style.display = 'none';
+            submitButton.style.display = 'inline-block';
+        } else {
+            nextButton.style.display = 'inline-block';
+            submitButton.style.display = 'none';
+        }
+    }
+
+    function showNextSlide() {
+        showSlide(currentSlide + 1);
+    }
+
+    function showPreviousSlide() {
+        showSlide(currentSlide - 1);
+    }
+
+    let previousButton = document.getElementById('previous');
+    let nextButton = document.getElementById('next');
+
+    previousButton.addEventListener('click', showPreviousSlide);
+    nextButton.addEventListener('click', showNextSlide);
+
+    showSlide(currentSlide);
+
+    submitButton.addEventListener('click', showResults);
+
+    function showResults() {
+        const answerContainers = document.querySelectorAll('.answers');
+        let numCorrect = 0;
+
+        quizQuestions.forEach((currentQuestion, questionNumber) => {
+            const selector = `input[name=question${questionNumber}]:checked`;
+            const userAnswer = (answerContainers[questionNumber].querySelector(selector) || {}).value;
+
+            if (userAnswer === currentQuestion.correctAnswer) {
+                numCorrect++;
+                alert(`Good Job! You got it right!`);
+            } else {
+                alert(`That is not the right answer, try again!`);
+            }
+        });
+
+        const resultsContainer = document.getElementById('result');
+        resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
+    }
 }
-
-const submitButton = document.getElementById('submit');
-submitButton.addEventListener('click', showResults);
-
 buildQuiz();
